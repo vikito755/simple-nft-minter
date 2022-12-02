665f7f379563ca0e85083968c1060290e4e7f461
Viktor Lazarov <vikitor755@gmail.com>
2022-12-02 14:38:05 -0800

    Load the minting function client-side.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import MintingForm from '../components/form'
+import MintingForm from '../components/form.client'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
