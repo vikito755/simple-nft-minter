1059c8b43f89b2d621f2d378237d0b5f204776b7
Viktor Lazarov <vikitor755@gmail.com>
2022-12-02 14:12:41 -0800

    Stable version of a simple NFT minter for the Goerli testnet.

---
+++ blockchain/mintNft.ts
@@ -0,0 +1,41 @@
+import { ethers } from "ethers";
+import toast from "react-hot-toast";
+import smartContractAbi from "./smartContractAbi"
+
+// Not important to the mint when testing.
+const arbitraryUri = '0';
+const nftQuantity = '1';
+const mintContractAddress = "0x60C9040c4809c1fC34247bC94683717377c5FAE2";
+
+
+const mintNft = async (_mintNftTo: string, _nftTitle: string, _imageUrl: string) => {
+
+
+    try {
+        const cryptoWindow: any = window;
+        const metamaskProvider = new ethers.providers.Web3Provider(
+          cryptoWindow.ethereum
+        );
+    
+          const signer = await metamaskProvider.getSigner();
+          const nftContract = await new ethers.Contract(
+            mintContractAddress,
+            smartContractAbi,
+            signer
+          );
+    
+          await nftContract["safeMint(address,string,string,string,string,uint8)"](
+            _mintNftTo,
+            arbitraryUri,
+            _nftTitle,
+            _nftTitle,
+            _imageUrl,
+            nftQuantity
+          );
+        }
+        catch {
+            toast.error('Failed to mint an NFT.')
+        };
+}
+
+export default mintNft;
---
+++ components/form.tsx
@@ -0,0 +1,87 @@
+import { useToggle, upperFirst } from '@mantine/hooks';
+import { useForm } from '@mantine/form';
+import {
+  TextInput,
+  PasswordInput,
+  Text,
+  Paper,
+  Group,
+  PaperProps,
+  Button,
+  Divider,
+  Checkbox,
+  Anchor,
+  Stack,
+} from '@mantine/core';
+import { FC } from 'react';
+import mintNft from '../blockchain/mintNft';
+import { ethers } from 'ethers';
+
+const MintingForm: FC = (props: PaperProps) => {
+  const form = useForm({
+    initialValues: {
+      address: '',
+      imageUrl: '',
+      nftName: ''
+    },
+
+    validate: {
+    //   address: (val) => (ethers.utils.isAddress(val) ? null : 'Invalid email'),
+    //   password: (val) => (val.length <= 6 ? 'Password should include at least 6 characters' : null),
+    },
+  });
+
+  return (
+    <Paper radius="md" p="xl" withBorder {...props}>
+      <Text size="lg" weight={500}>
+        Welcome to Simple Nft Minter on Goerli Testnet.
+      </Text>
+
+      <Divider label="Set your network to Goerli tesnet and unlock your wallet." labelPosition="center" my="lg" />
+
+      <form onSubmit={form.onSubmit(() => {})}>
+        <Stack>
+
+          <TextInput
+            required
+            label="address"
+            placeholder="0x123..."
+            value={form.values.address}
+            onChange={(event) => form.setFieldValue('address', event.currentTarget.value)}
+            error={form.errors.address && 'Invalid address'}
+          />
+
+            <TextInput
+            required
+            label="image URL"
+            placeholder="https://url-to-your-image"
+            value={form.values.imageUrl}
+            onChange={(event) => form.setFieldValue('imageUrl', event.currentTarget.value)}
+            error={form.errors.imageUrl && 'Invalid imageUrl'}
+          />
+
+          <TextInput
+            required
+            label="NFT name"
+            placeholder="Determined Squirrel Buddies"
+            value={form.values.nftName}
+            onChange={(event) => form.setFieldValue('nftName', event.currentTarget.value)}
+            error={form.errors.nftName && 'Invalid nft name'}
+          />
+        </Stack>
+
+        <Group position="apart" mt="xl">
+          
+          <Button onClick={ async () => {
+            const mintNft = (
+                await import('../blockchain/mintNft')
+                // await import("../../redux/nftsApi")
+              ).default;
+              mintNft(form.values.address, form.values.nftName, form.values.imageUrl)}} type="submit" fullWidth>Mint</Button>
+        </Group>
+      </form>
+    </Paper>
+  );
+}
+
+export default MintingForm;
